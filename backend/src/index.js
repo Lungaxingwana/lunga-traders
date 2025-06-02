b48a73ee316fe9450e968a7b39d2c946b04156e3
Lungaxingwana <97188625+Lungaxingwana@users.noreply.github.com>
2025-06-02 13:12:49 +0200

    update

--- backend/src/index.js
+++ backend/src/index.js
@@ -9,9 +9,11 @@ import invoiceRoutes from "./routes/invoice.route.js";
 import cookieParser from 'cookie-parser';
 import { app, server } from './libs/socket.js';
 import { connectAndRun } from './libs/db.js';
+import path from "path";
 dotenv.config();
 
 const PORT = process.env.PORT;
+const __dirname = path.resolve();
 
 app.use(cors({
     origin: ["http://localhost:3000"],
@@ -26,6 +28,14 @@ app.use("/api/auth", userRoutes);
 app.use("/api/products", productRoutes);
 app.use("/api/invoices", invoiceRoutes);
 app.use(morgan('dev'));
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 // Start server and connect to DB
 server.listen(PORT, () => {
